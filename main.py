fea072ae048ad1cf1bca0a1814f3d68e249fea5a
Andrew Wang <andrew_wang2002@hotmail.com>
2024-01-29 10:19:59 -0700

    #8 added export support

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
-# from controller import SensorCtrl
-# import time
-
-# # Connection to Snesors
-# ctrl = SensorCtrl(port="COM5", baud=9600, timeout=.1)
-
-# # Serial Monitor
-# while True:
-#     print(ctrl.read_from(), end="")
-#     time.sleep(1.5)
+from controller import SensorCtrl
 
 from PyQt5.QtWidgets import QApplication
 
 from main_gui import Ui_MainWindow
-import sys
+import sys, ctypes, os, time, re, atexit
+from pathlib import Path
 
 class Window(Ui_MainWindow):
 
@@ -21,7 +13,57 @@ class Window(Ui_MainWindow):
         self.setupUi(self)
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    win = Window()
-    win.show()
-    sys.exit(app.exec())
+    # # Update App ID if Windows
+    # myappid = 'medal.sensorfusion.h2'
+    # if os.name == "nt":
+    #     ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+
+    # # Prep App Launch
+    # app = QApplication(sys.argv)
+    # win = Window()
+    # win.show()
+    # sys.exit(app.exec())
+
+    # Make data directory if not already
+    Path("data/").mkdir(parents=True, exist_ok=True)
+
+    # Connection to Snesors
+    ctrl = SensorCtrl(port="COM5", baud=9600, timeout=.1)
+
+    # Look for export file
+    export = open(f"data/data-{int(time.time())}.csv", "w", encoding="utf-8")
+    atexit.register(lambda : export.close())
+    
+    # Grab latest data from sensors to make header in csv
+    row = ctrl.read_from()
+
+    # Parse Data row
+    data = re.search(r"([a-zA-Z ])Ω:(inf|\d+\.\d{2}),%RH:(inf|\d+\.\d{2}),°C:(inf|\d+\.\d{2})", row)
+    print(f"{data.group(2)} {data.group(1)}Ω | {data.group(3)}%RH | {data.group(4)}°C")
+
+    # Header
+    export.write(f'"Resistance ({data.group(1)}Ω)","Humidity (%RH)","Temperature (°C)"\n')
+
+    # Flush counter
+    f_counter = 1
+
+    # Serial Monitor
+    while True:
+        # Write last row of data
+        export.write(f'"{data.group(2)}","{data.group(3)}","{data.group(4)}"\n')
+        f_counter += 1
+
+        # Grab latest data from sensors
+        row = ctrl.read_from()
+
+        # Parse Data row
+        data = re.search(r"([a-zA-Z ])Ω:(inf|\d+\.\d{2}),%RH:(inf|\d+\.\d{2}),°C:(inf|\d+\.\d{2})", row)
+        print(f"{data.group(2)} {data.group(1)}Ω | {data.group(3)}%RH | {data.group(4)}°C")
+
+        # Flush every 50 rows
+        if f_counter >= 50:
+            export.flush()
+            f_counter = 0
+
+        # Delay a bit
+        time.sleep(1.5)
