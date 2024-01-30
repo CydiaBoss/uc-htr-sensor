4331c7103cccdcea5bd9e21d75f9d57ff58cc958
Andrew Wang <andrew_wang2002@hotmail.com>
2024-01-29 17:08:39 -0700

    #11 add live graph function

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from threading import Thread
 from controller import SensorCtrl
 
 from PyQt5.QtWidgets import QApplication
+
+from pglive.sources.data_connector import DataConnector
 
 from main_gui import Ui_MainWindow
 import sys, ctypes, os, time, re, atexit
@@ -12,6 +15,33 @@ class Window(Ui_MainWindow):
         super().__init__(parent)
         self.setupUi(self)
 
+def data_collection(ctrl : SensorCtrl, resist_data : DataConnector, humidity_data : DataConnector, temperature_data : DataConnector):
+    '''
+    Update the graphs on the GUI
+    '''
+    # Counter
+    x = 0
+    while True:
+        # Grab next row
+        row = ctrl.read_from()
+
+        # Parse Data row
+        data = re.search(r"([a-zA-Z ])Ω:(inf|\d+\.\d{2}),%RH:(inf|\d+\.\d{2}),°C:(inf|\d+\.\d{2})", row)
+
+        # Update Graph if not None
+        if data is not None:
+            # Ignore for inf
+            if data.group(2) != "inf":
+                resist_data.cb_append_data_point(float(data.group(2)), x)
+            humidity_data.cb_append_data_point(float(data.group(3)), x)
+            temperature_data.cb_append_data_point(float(data.group(4)), x)
+
+        # Delay a bit
+        time.sleep(1)
+
+        # Update x
+        x += 1
+
 if __name__ == "__main__":
     # Update App ID if Windows
     myappid = 'medal.sensorfusion.h2'
@@ -21,18 +51,20 @@ if __name__ == "__main__":
     # Prep App Launch
     app = QApplication(sys.argv)
     win = Window()
-    win.show()
 
     # Main Logic
+    # Connection to Sensors
+    ctrl = SensorCtrl(port="COM5", baud=9600, timeout=.1)
+
+    win.show()
+    
+    Thread(target=data_collection, args=(ctrl, win.resist_data, win.humidity_data, win.temperature_data)).start()
 
     # End
     sys.exit(app.exec())
 
     # Make data directory if not already
     Path("data/").mkdir(parents=True, exist_ok=True)
-
-    # Connection to Snesors
-    ctrl = SensorCtrl(port="COM5", baud=9600, timeout=.1)
 
     # Look for export file
     export = open(f"data/data-{int(time.time())}.csv", "w", encoding="utf-8")
