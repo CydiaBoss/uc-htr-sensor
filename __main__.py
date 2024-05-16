117d025a7e3e1ac263de1da9bca7ebc6b71bfd58
Andrew Wang <andrew_wang2002@hotmail.com>
2024-05-16 15:12:17 -0600

    #23 added a splash screen

--- __main__.py
+++ __main__.py
@@ -5,9 +5,12 @@ from multiprocessing import freeze_support
 from misc.constants import APPID
 from misc.logger import Logger
 
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QSplashScreen
 
 from main import Window
+
+import main_rc as _
 
 if __name__ == "__main__":
     # Freeze Support
@@ -17,16 +20,26 @@ if __name__ == "__main__":
     if sys.platform.startswith("win"):
         ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(APPID)
 
-    # Start Logger
-    Logger()
-
     # Prep App Launch
     app = QApplication(sys.argv)
 
+    # Show Splash
+    pixmap = QPixmap(":/main/uclogo.png")
+    splash = QSplashScreen(pixmap)
+    splash.show()
+
+    # Process
+    app.processEvents()
+
+    # Start Logger
+    Logger()
+
+    # Setup Window
     win = Window()
 
     # Show
     win.show()
+    splash.finish(win)
 
     # End
     sys.exit(app.exec())
